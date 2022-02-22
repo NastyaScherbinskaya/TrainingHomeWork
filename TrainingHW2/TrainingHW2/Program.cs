171c78575cd221f3fef7272ddae069967f800b17
NastyaScherbinskaya <sherbinskayanastya@gmail.com>
2022-02-22 14:27:33 +0300

    HW2

---
+++ TrainingHW2/TrainingHW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TrainingHW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Console.WriteLine("Элементы массива в квадрате: \n");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * i;
+                Console.WriteLine($"array[{i}] = {array[i]} \n");
+            }
+
+        }
+    }
+}
