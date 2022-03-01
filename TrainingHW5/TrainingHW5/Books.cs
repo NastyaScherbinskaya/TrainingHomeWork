a7cf444f27f3a2152ae5735e63d98e2f2d1ae020
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-01 23:23:13 +0300

    HW5

---
+++ TrainingHW5/TrainingHW5/Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingHW5
+{
+    class Books
+    {
+        public string bookname { get; }
+        public int amount_pages { get; }
+        public string Id { get; }
+
+    
+        public Books(string name, int amount, string id )
+        {
+            bookname = name;
+            amount_pages = amount;
+            Id = id;
+        }
+
+        public string Display_Books()
+
+        {
+            return String.Format($"Название книги - {bookname}, количество страниц - {amount_pages}, уникальный идентификатор - {Id} \n");
+        }
+
+
+    }
+
+}
