f177fe61d97dbb94a8e4dafebd93d7cb0d7462f4
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-11 12:12:32 +0300

    HW1_updated

---
+++ TrainingHW1/TrainingHW1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TrainingHW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Byte type:");
+            Console.WriteLine($"Minimal Byte value: {byte.MinValue}");
+            Console.WriteLine($"Maximum Byte value: {byte.MaxValue}\n");
+
+            Console.WriteLine("Short type:");
+            Console.WriteLine($"Minimal Short value: {short.MinValue}");
+            Console.WriteLine($"Maximum Short value: {short.MaxValue}\n");
+
+            Console.WriteLine("Int type:");
+            Console.WriteLine($"Minimal Int value: {int.MinValue}");
+            Console.WriteLine($"Maximum Int value: {int.MaxValue}\n");
+
+            Console.WriteLine("Long type:");
+            Console.WriteLine($"Minimal Long value: {long.MinValue}");
+            Console.WriteLine($"Maximum Long value: {long.MaxValue}\n");
+
+            Console.WriteLine("Float type:");
+            Console.WriteLine($"Minimal Float value: {float.MinValue}");
+            Console.WriteLine($"Maximum Float value: {float.MaxValue}\n");
+
+            Console.WriteLine("Double type:");
+            Console.WriteLine($"Minimal Double value: {double.MinValue}");
+            Console.WriteLine($"Maximum Double value: {double.MaxValue}\n");
+
+            Console.WriteLine("Uint type:");
+            Console.WriteLine($"Minimal Uint value: {uint.MinValue}");
+            Console.WriteLine($"Maximum Uint value: {uint.MaxValue}\n");
+
+            Console.WriteLine("ULong type:");
+            Console.WriteLine($"Minimal Ulong value: {ulong.MinValue}");
+            Console.WriteLine($"Maximum Ulong value: {ulong.MaxValue}\n");
+
+
+        }
+    }
+}
