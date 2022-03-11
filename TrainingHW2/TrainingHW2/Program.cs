4399efb2c6de48e52e07bea6778388adb0f2b51a
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-11 12:28:11 +0300

    HW2_updated

---
+++ TrainingHW2/TrainingHW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TrainingHW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Console.WriteLine("Array elements squared: \n");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * i;
+                Console.WriteLine($"array[{i}] = {array[i]} \n");
+            }
+
+        }
+    }
+}
