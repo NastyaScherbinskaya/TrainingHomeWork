4c2967bf6cc661ba11a9a9631ef5c28934cb053d
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-11 12:48:30 +0300

    HW3

---
+++ TrainingHW3/TrainingHW3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TrainingHW3
+{
+    class Program
+    {
+        static void ValuesForArray(int[] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (int)Math.Pow(i, value);
+
+            }
+        }
+
+        static float AverageSum(int[] array)
+        {
+            int SumArray = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                SumArray += array[i];
+
+            }
+            float AverageValue = (float)SumArray / array.Length;
+
+            return AverageValue;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[10];
+
+            for (int i = 1; i < 4; i++)
+            {
+                ValuesForArray(array, i);
+                Console.WriteLine($"Average array {i} value : {AverageSum(array)} ");
+
+            }
+
+        }
+    }
+}
