fe9ba68d7abdc9999d4f2bc06a1ffdd54e8dde51
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-11 10:34:53 +0300

    HW6

---
+++ TrainingHW6/TrainingHW6/Buyer_credit_card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingHW6
+{
+    class Buyer_credit_card : Buyer
+    {
+
+        public override string Pay()
+
+        {
+            return "Покупатель с кредитной картой";
+        }
+    }
+}
---
+++ TrainingHW6/TrainingHW6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TrainingHW6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 1;
+
+            Buyer buyer1 = new Buyer_cash();
+            Buyer buyer2 = new Buyer_credit_card();
+            Buyer buyer3 = new Buyer_cash();
+            Buyer buyer4 = new Buyer_debit_card();
+            Buyer buyer5 = new Buyer_debit_card();
+
+            Buyer[] buyer = { buyer1, buyer2, buyer3, buyer4, buyer5 };
+
+            foreach (Buyer item in buyer)
+
+            {
+                Console.WriteLine($"Покупатель {number} - {item.Pay()}\n");
+                number++;
+            }
+
+           
+        }
+    }
+}
