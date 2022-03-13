1de0929e5c646da85a66095b521233e938dbef48
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-03-13 23:14:15 +0300

    HW4_updated

--- TrainingHW4/TrainingHW4/Program.cs
+++ TrainingHW4/TrainingHW4/Program.cs
@@ -4,51 +4,13 @@ namespace TrainingHW4
 {
     class Program
     {
-        static int FindMinValue(int[] array)
+        static void FindMinMaxValue(int[] array)
         {
             int Min = array[0];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] < Min)
-                {
-                    Min = array[i];
-                }
-
-            }
-
-
-            return Min;
-        }
-
-        static int FindMaxValue(int[] array)
-        {
             int Max = array[0];
 
-
             for (int i = 0; i < array.Length; i++)
             {
-
-                if (array[i] > Max)
-                {
-                    Max = array[i];
-                }
-
-            }
-
-            return Max;
-
-        }
-
-
-        static void ChangeMax_and_Min(int[] array)
-        {
-            int Max = array[0];
-            int Min = array[0];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-
                 if (array[i] > Max)
                 {
                     Max = array[i];
@@ -57,64 +19,61 @@ namespace TrainingHW4
                 {
                     Min = array[i];
                 }
-
-
             }
+            Console.WriteLine($"\n Max value : {Max}, Min value : {Min}\n");
+        }
 
+        static void ChangeMaxMin(int[] array)
+        {
+            int Max = array[0];
+            int Min = array[0];
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > Max)
+                {
+                    Max = array[i];
+                }
+                if (array[i] < Min)
+                {
+                    Min = array[i];
+                }
+            }
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == Max)
                 {
                     array[i] = Min;
                 }
-
                 else if (array[i] == Min)
                 {
                     array[i] = Max;
                 }
             }
-
-
         }
 
-
+        static void OutputChangedArray(int[] array)
+        {
+            Console.WriteLine("Changed array:\n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
 
         static void Main(string[] args)
-
         {
-
             int[] array1 = { 1, 4, 5, 7, 5, -2, 56 };
-            Console.WriteLine($"Максимальное значение array1: {FindMaxValue(array1)} \n");
-            Console.WriteLine($"Минимальное значение array1: {FindMinValue(array1)} \n");
-            ChangeMax_and_Min(array1);
 
-            Console.WriteLine("Измененный array1:\n");
-            for (int i = 0; i < array1.Length; i++)
-            {
-                Console.WriteLine(array1[i]);
-
-            }
-
-
+            FindMinMaxValue(array1);
+            ChangeMaxMin(array1);
+            OutputChangedArray(array1);
 
             int[] array2 = { 0, 4, 9, 33, 3, -6, 15, 3, 25 };
-
-            Console.WriteLine($"\nМаксимальное значение array2: {FindMaxValue(array2)} \n");
-            Console.WriteLine($"Минимальное значение array2: {FindMinValue(array2)} \n");
-
-            ChangeMax_and_Min(array2);
-
-
-            Console.WriteLine("Измененный array2:\n");
-            for (int i = 0; i < array2.Length; i++)
-            {
-                Console.WriteLine(array2[i]);
-
-            }
-
-
-
+    
+            FindMinMaxValue(array2);
+            ChangeMaxMin(array2);
+            OutputChangedArray(array2);
         }
     }
 }
