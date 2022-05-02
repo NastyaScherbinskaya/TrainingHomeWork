e627e6ba3e8a3d7028c9185162ad628fcb91b79c
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-05-02 16:27:46 +0300

    Task20

---
+++ Selenium_Task20/TestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium_Task20;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public IWebDriver driver;
+
+        [TestInitialize]
+        public void BeginTest()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.yandex.com";
+        }
+
+        [TestCleanup]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void SuccessfulLogin()
+        {
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = homePage.GoToLoginPage();
+
+            var username = "kostyukevitch-marija";
+            var password = "kostyukevitch_marija";
+
+            loginPage.GoToCabinet(username, password);
+
+            Assert.IsTrue(driver.FindElement(By.XPath("//span[@class='avatar__image  avatar__image-server0']")).Displayed);
+        }
+    }
+
+    //Class for Task2
+    public class Variables
+    {
+        //Password field on Login page 
+        By Xpath = By.XPath("//input[@type='password']");
+        By id = By.Id("passp-field-passwd");
+        By name = By.ClassName("Textinput-Control");
+        By nane = By.Name("passwd");
+        By sss = By.CssSelector("#passp-field-passwd");
+    }
+}
+
