99018d97ab3e747ba48f32c30b9ea005dbf9d4b8
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-05-16 14:28:15 +0300

    commit

---
+++ TrainingHW11_Selenium/TrainingHW11_Selenium/CatalogPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TrainingHW11_Selenium
+{
+    public class CatalogPage
+    {
+        string CatalogTitleLocator = "//div[@class='catalog-navigation__title']";
+
+        public IWebDriver driver;
+        public IWebElement CatalogTitle { get; set; }
+
+        public CatalogPage(IWebDriver _driver)
+        {
+            driver = _driver;
+            CatalogTitle = driver.FindElement(By.XPath(CatalogTitleLocator));
+        }
+
+        public string GetCatalogTitle()
+        {
+            return CatalogTitle.Text.ToString();
+        }
+    }
+}
