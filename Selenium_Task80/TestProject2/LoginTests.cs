e8062506b1647174388e1993a7cdec55eb733e46
Nastya Scherbinskaya <sherbinskayanastya@gmail.com>
2022-05-20 17:31:04 +0300

    80Task

---
+++ Selenium_Task80/TestProject2/LoginTests.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Selenium_Task80;
+using System.Threading;
+using System;
+
+namespace LoginTests
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void BeginTest()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.yandex.com";
+            Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenshot.SaveAsFile(@"C:\Users\nastyascherbinskaya\Desktop\Screenshot.jpg");
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        [TestCase("kostyukevitch-marija", "kostyukevitch_marija")]
+        public void SuccessfulLogin(string username, string password)
+        {
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = homePage.GoToLoginPage();
+
+            loginPage.GoToCabinet(username, password);
+
+            Assert.IsTrue(driver.FindElement(By.XPath("//span[@class='avatar__image  avatar__image-server0']")).Displayed);
+        }
+
+        [Test]
+        [TestCase("kostyukevitch-marija", "kostyukevitch_marija")]
+        public void LogOut(string username, string password)
+        {
+            HomePage homePage = new HomePage(driver);
+            LoginPage loginPage = homePage.GoToLoginPage();
+
+            loginPage.GoToCabinet(username, password);
+            loginPage.Logout();
+            Assert.IsTrue(driver.FindElement(By.XPath("//div[contains(text(), 'Войти')]")).Displayed);
+        }
+    }
+}
